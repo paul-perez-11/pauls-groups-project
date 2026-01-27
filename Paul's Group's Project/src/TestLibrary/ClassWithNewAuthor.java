8083c2bf9659ef36f14385115a5da0e80223decb
Stellarr2 <pauljoel11perez@gmail.com>
2026-01-27 17:18:37 +0800

    Testing my new user for NetBeans

---
+++ Paul's Group's Project/src/TestLibrary/ClassWithNewAuthor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TestLibrary;
+
+/**
+ *
+ * @author Paul Joel D. Perez <pjdperez@mymail.mapua.edu.ph>
+ */
+public class ClassWithNewAuthor {
+    
+}
